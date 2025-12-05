154ca7161769c3c6e3d466b1708d69f513a4c1cc
Houston Braga <houstonliverboy123@gmail.com>
2025-12-05 01:56:31 -0300

    feat: remove cursor-point in faq item all

--- src/components/faq-item.tsx
+++ src/components/faq-item.tsx
@@ -13,12 +13,13 @@ const FaqItem = ({ question, response }: FaqItemProps) => {
 
   return (
     <div className="flex flex-col bg-linear-to-r to-45% from-primary-green to-secondary-blue rounded-3xl w-full">
-      <div
-        className="flex justify-between p-8 cursor-pointer transition-all"
-        onClick={() => setIsActive((prev) => !prev)}
-      >
+      <div className="flex justify-between p-8">
         <h2 className="text-white text-xl font-bold">{question}</h2>
-        <button type="button" className="cursor-pointer">
+        <button
+          type="button"
+          className="cursor-pointer"
+          onClick={() => setIsActive((prev) => !prev)}
+        >
           <CirclePlus
             className={`font-bold w-8 h-8 text-white transition-all ${
               isActive ? "rotate-45" : ""
@@ -27,7 +28,7 @@ const FaqItem = ({ question, response }: FaqItemProps) => {
         </button>
       </div>
       {isActive && (
-        <div className="px-8 pb-8 text-zinc-300 ">
+        <div className="px-8 pb-8 text-zinc-300">
           <p className="font-light ">{response}</p>
         </div>
       )}
