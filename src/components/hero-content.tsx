43edaca199df5e87403bae4e05eaeff4ac9bc6fc
Houston Braga <houstonliverboy123@gmail.com>
2025-12-06 02:04:55 -0300

    feat: add responsive layout on hero-content

--- src/components/hero-content.tsx
+++ src/components/hero-content.tsx
@@ -6,8 +6,8 @@ import ArrowDown from "@/assets/arrowdownhome.svg";
 const HeroContent = () => {
   return (
     <>
-      <LayoutContent className="flex mb-32 mt-52 flex-col text-center justify-center items-center text-white">
-        <h1 className="text-6xl font-bold max-w-4xl leading-normal">
+      <LayoutContent className="flex mb-32 mt-36 md:mt-52 flex-col text-center justify-center items-center text-white">
+        <h1 className="text-4xl md:text-6xl font-bold max-w-4xl leading-normal">
           O MAIOR ECOSSISTEMA DE VAREJO{" "}
           <span className="text-secondary-blue">SUPERMERCADISTA</span> DO BRASIL
         </h1>
@@ -40,7 +40,7 @@ const HeroContent = () => {
           />
         </svg>
         <LayoutContent className="bg-white flex justify-center items-center pb-28 text-center">
-          <p className="max-w-3xl text-xl font-semibold">
+          <p className="max-w-3xl md:text-xl text-md font-semibold md:pt-0 pt-12">
             Na Impacte, os desafios do dia a dia se transformam em oportunidades
             práticas de evolução  com apoio estratégico, comunidade ativa e
             soluções integradas que entregam resultado real.
