42d0c512d962f9e5567bbb19c036c88a1febb81a
Houston Braga <houstonliverboy123@gmail.com>
2025-12-28 01:26:19 -0300

    feat: add flutter animation to the gira photos component

--- src/components/events/photos-gira.tsx
+++ src/components/events/photos-gira.tsx
@@ -1,10 +1,10 @@
 const PhotosGira = () => {
   return (
     <div className="flex flex-wrap justify-center w-full gap-8">
-      <div className="w-[250px] h-[216px] rounded-4xl bg-zinc-300"></div>
-      <div className="w-[250px] h-[216px] rounded-4xl mt-8 bg-zinc-300"></div>
-      <div className="w-[250px] h-[216px] rounded-4xl bg-zinc-300"></div>
-      <div className="w-[250px] h-[216px] rounded-4xl mt-8 bg-zinc-300"></div>
+      <div className="w-[250px] h-[216px] rounded-4xl bg-zinc-300 flutuando animation-delay-0"></div>
+      <div className="w-[250px] h-[216px] rounded-4xl mt-8 bg-zinc-300 flutuando animation-delay-1"></div>
+      <div className="w-[250px] h-[216px] rounded-4xl bg-zinc-300 flutuando animation-delay-2"></div>
+      <div className="w-[250px] h-[216px] rounded-4xl mt-8 bg-zinc-300 flutuando animation-delay-3"></div>
     </div>
   );
 };
