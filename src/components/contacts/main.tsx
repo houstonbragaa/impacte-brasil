4e8ccc831e75078171067c30d3f9e950312bcb04
Houston Braga <houstonliverboy123@gmail.com>
2026-01-04 18:57:54 -0300

    refactor: adjuste in the contact form position

--- src/components/contacts/main.tsx
+++ src/components/contacts/main.tsx
@@ -5,7 +5,7 @@ import SocialMedias from "./social-medias";
 const Main = () => {
   return (
     <LayoutContent className="flex flex-col justify-center items-center relative z-10">
-      <LayoutDesktop className="flex flex-col items-center justify-center w-2xl mt-32 p-8 rounded-xl">
+      <LayoutDesktop className="flex flex-col items-center justify-center w-2xl p-8 rounded-xl">
         <ContactForm />
         <SocialMedias />
       </LayoutDesktop>
