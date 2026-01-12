82b75631cd0d8a03175f22b0a4b08d6a38798bd8
Houston Braga <houstonliverboy123@gmail.com>
2026-01-12 01:14:28 -0300

    feat: add constant and types to receive the testimonials data

---
+++ src/constants/testimonials.ts
@@ -0,0 +1,18 @@
+const testimonials = [
+  {
+    id: 1,
+    name: "Thayná",
+    supermarket: "União Supermarket",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis perferendis sit corporis soluta incidunt esse iste nulla excepturixs mollitia debitis cupiditate et.",
+    avatar: "djad"
+  },
+  {
+    id: 2,
+    name: "Houston",
+    supermarket: "djsdkajskd",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis perferendis sit corporis soluta incidunt esse iste nulla excepturixs mollitia debitis cupiditate et.",
+    avatar: "djad"
+  },
+];
+
+export default testimonials;
---
+++ src/types/home-types.ts
@@ -0,0 +1,11 @@
+interface TestimonialsTypes {
+  id?: number
+  name: string
+  supermarket: string
+  text: string
+  avatar: string
+}
+
+export type {
+  TestimonialsTypes
+}
