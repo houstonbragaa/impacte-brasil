c32667045a22fb0678a41418eb3cc629c297503d
Houston Braga <houstonliverboy123@gmail.com>
2026-01-14 14:59:07 -0300

    refactor: edit footer links

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -45,40 +45,64 @@ const Footer = () => {
           <div className="flex flex-col justify-start">
             <h2 className="mb-4 md:text-2xl text-xl font-normal">Endereço</h2>
             <div className="flex flex-col gap-2">
-              <h3 className="text-gray-400 md:text-xl text-md">Rod. Admar Gonzaga, 440</h3>
+              <h3 className="text-gray-400 md:text-xl text-md">
+                Rod. Admar Gonzaga, 440
+              </h3>
               <h3 className="text-gray-400 md:text-xl text-md">
                 Torre Pacifico – Itacorubi
               </h3>
-              <h3 className="text-gray-400 md:text-xl text-mdl">Florianópolis/SC</h3>
-            </div>
-          </div>
-
-          <div className="flex flex-col justify-start">
-            <h2 className="mb-4 md:text-2xl text-xl font-normal">Ecossistema</h2>
-            <div className="flex flex-col gap-2">
-              <Link href="/" className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md">
-                Sobre nós
-              </Link>
-              <Link href="/" className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md">
-                Gira comunidade
-              </Link>
-              <Link href="/" className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md">
-                Suporte
-              </Link>
+              <h3 className="text-gray-400 md:text-xl text-mdl">
+                Florianópolis/SC
+              </h3>
             </div>
           </div>
 
           <div className="flex flex-col justify-start">
             <h2 className="mb-4 md:text-2xl text-xl font-normal">Contatos</h2>
             <div className="flex flex-col gap-2">
-              <Link href="/" className="text-gray-400 hover:text-gray-400/50 md:text-xl text-mdl">
-                Instagram
+              <Link
+                href="/"
+                className="text-gray-400 cursor-text  md:text-xl text-mdl"
+              >
+                48 3036 2728
               </Link>
-              <Link href="/" className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md">
-                LinkedIn
-              </Link>
-              <Link href="/" className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md">
+              <Link
+                href="/"
+                className="text-gray-400 hover:text-gray-400/50  md:text-xl text-md"
+              >
                 WhatsApp
+              </Link>
+              <Link
+                href="/"
+                className="text-gray-400 cursor-text  md:text-xl text-md"
+              >
+                contato@impactebrasil.com.br
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-start">
+            <h2 className="mb-4 md:text-2xl text-xl font-normal">
+              Ecossistema
+            </h2>
+            <div className="flex flex-col gap-2">
+              <Link
+                href="/"
+                className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md"
+              >
+                Sobre nós
+              </Link>
+              <Link
+                href="/"
+                className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md"
+              >
+                Gira comunidade
+              </Link>
+              <Link
+                href="/"
+                className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md"
+              >
+                Formulário
               </Link>
             </div>
           </div>
@@ -86,10 +110,16 @@ const Footer = () => {
           <div className="flex flex-col justify-start">
             <h2 className="mb-4 md:text-2xl text-xl font-normal">Políticas</h2>
             <div className="flex flex-col gap-2">
-              <Link href="/" className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md">
+              <Link
+                href="/"
+                className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md"
+              >
                 Privacidade
               </Link>
-              <Link href="/" className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md">
+              <Link
+                href="/"
+                className="text-gray-400 hover:text-gray-400/50 md:text-xl text-md"
+              >
                 Termos de uso
               </Link>
             </div>
