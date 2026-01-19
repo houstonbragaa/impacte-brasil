c0fef06630d97a4780cc5482894a2760e593ce5b
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-19 19:54:48 -0300

    feat: add link to landing page gira comunidade in events button

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 const Button = ({ text }: ButtonProps) => {
   return (
-    <Link href="https://wa.me/554898060177" className="w-fit px-4 bg-secondary-blue h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-xs shadow-white hover:scale-105 hover:shadow-sm before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-linear-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-white">
+    <Link href="https://wa.me/554898060177" target="_blank" className="w-fit px-4 bg-secondary-blue h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-xs shadow-white hover:scale-105 hover:shadow-sm before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-linear-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-white">
       {text}
     </Link>
   );
--- src/components/events/video-gira.tsx
+++ src/components/events/video-gira.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import Button from "../common/button";
 import { LayoutContent, LayoutDesktop } from "../layout/layout-content";
 import Image from "next/image";
 import Glow from "../common/glow";
+import Link from "next/link";
 
 const VideoGira = () => {
   const [showVideo, setShowVideo] = useState<boolean>(false);
@@ -54,7 +54,9 @@ const VideoGira = () => {
           Mais do que comunidade: um ecossistema vivo, com gente que fala a
           mesma língua, sonha grande e age junto.
         </p>
-        <Button text="Conhecer melhor nossa comunidade" />
+        <Link href="https://comunidadegira.com.br/" target="_blank" className="w-fit px-4 bg-secondary-blue h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-xs shadow-white hover:scale-105 hover:shadow-sm before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-linear-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-white">
+          Conheça melhor nossa comunidade
+        </Link>
       </LayoutDesktop>
     </LayoutContent>
   );
