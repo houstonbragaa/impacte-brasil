d77abc094744aea492672afa9e2afeab3abad3df
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-19 22:01:29 -0300

    feat: add terms page

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -20,123 +20,123 @@ const Privacy = () => {
                 coletamos, utilizamos, armazenamos e protegemos informações, em conformidade com a
                 Lei Geral de Proteção de Dados (Lei nº 13.709/2018 – LGPD).</p>
 
-                <h2>1. Dados Coletados</h2>
+                <h2 className="text-secondary-blue text-2xl">1. Dados Coletados</h2>
 
-                <p>Podemos coletar e tratar os seguintes dados:</p>
+                <p className="text-white/90 text-xl">Podemos coletar e tratar os seguintes dados:</p>
 
-                <h3> a) Dados pessoais</h3>
+                <h3 className="text-secondary-blue text-xl"> a) Dados pessoais</h3>
                 <ul className="ml-6">
-                  <li>●  Nome completo</li>
-                  <li>●  E-mail</li>
-                  <li>●  Telefone</li>
-                  <li>●  Cargo e empresa</li>
-                  <li>●  Dados fornecidos em formulários, diagnósticos, eventos, conteúdos e comunidades</li>
+                  <li className="text-white/90 text-xl">●  Nome completo</li>
+                  <li className="text-white/90 text-xl">●  E-mail</li>
+                  <li className="text-white/90 text-xl">●  Telefone</li>
+                  <li className="text-white/90 text-xl">●  Cargo e empresa</li>
+                  <li className="text-white/90 text-xl">●  Dados fornecidos em formulários, diagnósticos, eventos, conteúdos e comunidades</li>
                 </ul>
 
-                <h3>b) Dados estratégicos empresariais</h3>
+                <h3 className="text-secondary-blue text-xl">b) Dados estratégicos empresariais</h3>
                 <ul className="ml-6">
-                  <li>●  Informações operacionais, financeiras, gerenciais e estratégicas fornecidas voluntariamente</li>
-                  <li>●  Respostas a diagnósticos, avaliações, formulários e ferramentas como o ScanCorp</li>
-                  <li>●  Dados compartilhados em eventos, mentorias, comunidades e interações
+                  <li className="text-white/90 text-xl">●  Informações operacionais, financeiras, gerenciais e estratégicas fornecidas voluntariamente</li>
+                  <li className="text-white/90 text-xl">●  Respostas a diagnósticos, avaliações, formulários e ferramentas como o ScanCorp</li>
+                  <li className="text-white/90 text-xl">●  Dados compartilhados em eventos, mentorias, comunidades e interações
                   estratégicas</li>
                 </ul>
 
-                <h3>c) Dados de navegação</h3>
+                <h3 className="text-secondary-blue text-xl">c) Dados de navegação</h3>
                 <ul className="ml-6">
-                  <li>●  Endereço IP</li>
-                  <li>●  Tipo de navegador e dispositivo</li>
-                  <li>●  Páginas acessadas</li>
-                  <li>●  Cookies e tecnologias similares</li>
+                  <li className="text-white/90 text-xl">●  Endereço IP</li>
+                  <li className="text-white/90 text-xl">●  Tipo de navegador e dispositivo</li>
+                  <li className="text-white/90 text-xl">●  Páginas acessadas</li>
+                  <li className="text-white/90 text-xl">●  Cookies e tecnologias similares</li>
                 </ul>
 
-                <h2>2. Finalidade do Tratamento</h2>
+                <h2 className="text-secondary-blue text-2xl">2. Finalidade do Tratamento</h2>
 
-                <p>Os dados coletados são utilizados para:</p>
+                <p className="text-white/90 text-xl">Os dados coletados são utilizados para:</p>
 
                 <ul className="ml-6">
-                  <li>●  Contato institucional e comercial</li>
-                  <li>●  Envio de conteúdos, materiais, convites e comunicações</li>
-                  <li>●  Execução de diagnósticos, análises e recomendações estratégicas</li>
-                  <li>●  Gestão do relacionamento com clientes, leads e membros da comunidade</li>
-                  <li>●  Melhoria contínua de produtos, serviços e experiências</li>
-                  <li>●  Cumprimento de obrigações legais e regulatórias</li>
+                  <li className="text-white/90 text-xl">●  Contato institucional e comercial</li>
+                  <li className="text-white/90 text-xl">●  Envio de conteúdos, materiais, convites e comunicações</li>
+                  <li className="text-white/90 text-xl">●  Execução de diagnósticos, análises e recomendações estratégicas</li>
+                  <li className="text-white/90 text-xl">●  Gestão do relacionamento com clientes, leads e membros da comunidade</li>
+                  <li className="text-white/90 text-xl">●  Melhoria contínua de produtos, serviços e experiências</li>
+                  <li className="text-white/90 text-xl">●  Cumprimento de obrigações legais e regulatórias</li>
                 </ul>
 
-                <p>A Impacte Brasil <b className="text-secondary-blue">não comercializa dados pessoais ou empresariais.</b></p>
+                <p className="text-white/90 text-xl">A Impacte Brasil <b className="text-secondary-blue">não comercializa dados pessoais ou empresariais.</b></p>
 
-                <h2>3. Dados Estratégicos e Anonimização</h2>
+                <h2 className="text-secondary-blue text-2xl">3. Dados Estratégicos e Anonimização</h2>
 
-                <p>Os dados estratégicos empresariais:</p>
+                <p className="text-white/90 text-xl">Os dados estratégicos empresariais:</p>
                 
                 <ul className="ml-6">
-                  <li>●  São fornecidos voluntariamente pelos usuários</li>
-                  <li>●  São utilizados para análises internas, diagnósticos e geração de valor estratégico</li>
-                  <li><b className="text-secondary-blue">●  Nunca são compartilhados de forma identificável com terceiros</b></li>
+                  <li className="text-white/90 text-xl">●  São fornecidos voluntariamente pelos usuários</li>
+                  <li className="text-white/90 text-xl">●  São utilizados para análises internas, diagnósticos e geração de valor estratégico</li>
+                  <li className="text-white/90 text-xl"><b className="text-secondary-blue">●  Nunca são compartilhados de forma identificável com terceiros</b></li>
                 </ul>
 
-                <p>A Impacte Brasil pode utilizar dados anonimizados e agregados para fins:</p>
+                <p className="text-white/90 text-xl">A Impacte Brasil pode utilizar dados anonimizados e agregados para fins:</p>
                 <ul className="ml-6">
-                  <li>●  Estatísticos</li>
-                  <li>●  Educacionais</li>
-                  <li>●  Produção de conteúdos, estudos e benchmarks
+                  <li className="text-white/90 text-xl">●  Estatísticos</li>
+                  <li className="text-white/90 text-xl">●  Educacionais</li>
+                  <li className="text-white/90 text-xl">●  Produção de conteúdos, estudos e benchmarks
                   Sem qualquer identificação de empresas ou pessoas.</li>
                 </ul>
 
-                <h2>4. Base Legal</h2>
+                <h2 className="text-secondary-blue text-2xl">4. Base Legal</h2>
 
-                <p>O tratamento de dados ocorre com base em:</p>
+                <p className="text-white/90 text-xl">O tratamento de dados ocorre com base em:</p>
                 <ul className="ml-6">
-                  <li>●  Consentimento do titular</li>
-                  <li>●  Execução de contrato ou procedimentos preliminares</li>
-                  <li>●  Legítimo interesse, respeitando direitos e liberdades fundamentais</li>
+                  <li className="text-white/90 text-xl">●  Consentimento do titular</li>
+                  <li className="text-white/90 text-xl">●  Execução de contrato ou procedimentos preliminares</li>
+                  <li className="text-white/90 text-xl">●  Legítimo interesse, respeitando direitos e liberdades fundamentais</li>
                 </ul>
 
-                <h2>5. Uso de CRM, Automação e Anúncios</h2>
+                <h2 className="text-secondary-blue text-2xl">5. Uso de CRM, Automação e Anúncios</h2>
 
-                <p>Utilizamos plataformas de CRM, automação de marketing e gestão comercial para:</p>
+                <p className="text-white/90 text-xl">Utilizamos plataformas de CRM, automação de marketing e gestão comercial para:</p>
                 <ul className="ml-6">
-                  <li>●  Armazenar histórico de interações</li>
-                  <li>●  Organizar comunicações</li>
-                  <li>●  Realizar contatos ativos e campanhas segmentadas</li>
+                  <li className="text-white/90 text-xl">●  Armazenar histórico de interações</li>
+                  <li className="text-white/90 text-xl">●  Organizar comunicações</li>
+                  <li className="text-white/90 text-xl">●  Realizar contatos ativos e campanhas segmentadas</li>
                 </ul>
-                <p>Os dados também podem ser originados de campanhas patrocinadas (anúncios pagos),
+                <p className="text-white/90 text-xl">Os dados também podem ser originados de campanhas patrocinadas (anúncios pagos),
                 respeitando as políticas das plataformas e a legislação vigente.</p>
-                <p>O titular pode solicitar descadastramento a qualquer momento.</p>
+                <p className="text-white/90 text-xl">O titular pode solicitar descadastramento a qualquer momento.</p>
 
-                <h2>6. Compartilhamento de Dados</h2>
-                <p>Os dados poderão ser compartilhados apenas com:</p>
+                <h2 className="text-secondary-blue text-2xl">6. Compartilhamento de Dados</h2>
+                <p className="text-white/90 text-xl">Os dados poderão ser compartilhados apenas com:</p>
                 <ul className="ml-6">
-                  <li>●  Fornecedores de tecnologia e plataformas essenciais à operação</li>
-                  <li>●  Parceiros estratégicos diretamente envolvidos na entrega das soluções</li>
-                  <li>●  Autoridades legais, quando exigido</li>
+                  <li className="text-white/90 text-xl">●  Fornecedores de tecnologia e plataformas essenciais à operação</li>
+                  <li className="text-white/90 text-xl">●  Parceiros estratégicos diretamente envolvidos na entrega das soluções</li>
+                  <li className="text-white/90 text-xl">●  Autoridades legais, quando exigido</li>
                 </ul>
-                <p>Sempre observando confidencialidade, finalidade e segurança.</p>
+                <p className="text-white/90 text-xl">Sempre observando confidencialidade, finalidade e segurança.</p>
 
-                <h2>7. Armazenamento e Segurança</h2>
-                <p>Adotamos medidas técnicas e administrativas adequadas para proteger os dados contra:</p>
+                <h2 className="text-secondary-blue text-2xl">7. Armazenamento e Segurança</h2>
+                <p className="text-white/90 text-xl">Adotamos medidas técnicas e administrativas adequadas para proteger os dados contra:</p>
                 <ul className="ml-6">
-                  <li>●  Acesso não autorizado</li>
-                  <li>●  Perda, destruição ou alteração indevida</li>
+                  <li className="text-white/90 text-xl">●  Acesso não autorizado</li>
+                  <li className="text-white/90 text-xl">●  Perda, destruição ou alteração indevida</li>
                 </ul>
-                <p>Os dados são armazenados apenas pelo tempo necessário às finalidades descritas.</p>
+                <p className="text-white/90 text-xl">Os dados são armazenados apenas pelo tempo necessário às finalidades descritas.</p>
 
-                <h2>8. Direitos do Titular</h2>
-                <p>O titular pode solicitar:</p>
+                <h2 className="text-secondary-blue text-2xl">8. Direitos do Titular</h2>
+                <p className="text-white/90 text-xl">O titular pode solicitar:</p>
                 <ul className="ml-6">
-                  <li>●  Confirmação da existência de tratamento</li>
-                  <li>●  Acesso aos dados</li>
-                  <li>●  Correção de dados incompletos ou desatualizados</li>
-                  <li>●  Exclusão, anonimização ou bloqueio</li>
-                  <li>●  Revogação do consentimento</li>
+                  <li className="text-white/90 text-xl">●  Confirmação da existência de tratamento</li>
+                  <li className="text-white/90 text-xl">●  Acesso aos dados</li>
+                  <li className="text-white/90 text-xl">●  Correção de dados incompletos ou desatualizados</li>
+                  <li className="text-white/90 text-xl">●  Exclusão, anonimização ou bloqueio</li>
+                  <li className="text-white/90 text-xl">●  Revogação do consentimento</li>
                 </ul>
-                <p>Solicitações devem ser enviadas para: <b className="text-secondary-blue">contato@impactebrasil.com.br</b></p>
+                <p className="text-white/90 text-xl">Solicitações devem ser enviadas para: <b className="text-secondary-blue">contato@impactebrasil.com.br</b></p>
 
-                <h2>9. Cookies</h2>
-                <p>Utilizamos cookies para melhorar a experiência do usuário. O gerenciamento pode ser feito
+                <h2 className="text-secondary-blue text-2xl">9. Cookies</h2>
+                <p className="text-white/90 text-xl">Utilizamos cookies para melhorar a experiência do usuário. O gerenciamento pode ser feito
                 diretamente no navegador.</p>
                 
-                <h2>10. Alterações</h2>
-                <p>Esta Política pode ser atualizada a qualquer momento. Recomendamos revisão periódica.</p>
+                <h2 className="text-secondary-blue text-2xl">10. Alterações</h2>
+                <p className="text-white/90 text-xl">Esta Política pode ser atualizada a qualquer momento. Recomendamos revisão periódica.</p>
               </div>
             </LayoutDesktop>
           </LayoutContent>
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,6 +1,63 @@
+import { LayoutContent, LayoutDesktop } from "@/components/layout/layout-content"
+import styles from '@/app/styles.module.css'
+
+import '../guidelines.css'
+import Footer from "@/components/common/footer"
+
 const Terms = () => {
   return (
-    <h1>Termos</h1>
+    <>
+      <div className="flex flex-col justify-center items-center overflow-x-hidden">
+        <div
+          className={`flex flex-col w-full relative ${styles.bgGradientHero}`}
+        >
+          <LayoutContent className="md:mt-24 mt-12 md:mb-24 mb-12">
+            <LayoutDesktop className="flex flex-col justify-center items-center overflow-x-hidden">
+              <h1 className="text-4xl mb-12">Termos de uso</h1>
+              <div className="space-y-8 ">
+                <p className="text-white/90 text-xl">Ao acessar o site da Impacte Brasil, o usuário concorda integralmente com estes Termos de
+                Uso.</p>
+
+                <h2 className="text-secondary-blue text-2xl">1. Finalidade do Site</h2>
+                <p className="text-white/90 text-xl">O site tem como objetivo:</p>
+                <ul className="ml-6">
+                  <li className="text-white/90 text-xl">●  Disponibilizar informações institucionais</li>
+                  <li className="text-white/90 text-xl">●  Oferecer conteúdos educacionais e estratégicos</li>
+                  <li className="text-white/90 text-xl">●  Divulgar eventos, programas, comunidades e soluções</li>
+                  <li className="text-white/90 text-xl">●  Facilitar contato e relacionamento com o ecossistema Impacte Brasil</li>
+                </ul>
+
+                <h2 className="text-secondary-blue text-2xl">2. Uso Adequado</h2>
+                <p className="text-white/90 text-xl">O usuário compromete-se a:</p>
+                <ul className="ml-6">
+                  <li className="text-white/90 text-xl">●  Utilizar o site de forma lícita e ética</li>
+                  <li className="text-white/90 text-xl">●  Não violar direitos de terceiros</li>
+                  <li className="text-white/90 text-xl">●  Não tentar acessar áreas restritas ou sistemas internos</li>
+                </ul>
+
+                <h2 className="text-secondary-blue text-2xl">3. Propriedade Intelectual</h2>
+                <p className="text-white/90 text-xl">Todo o conteúdo do site pertence à Impacte Brasil ou a seus parceiros, sendo proibida a
+                reprodução, distribuição ou uso comercial sem autorização expressa.</p>
+              
+                <h2 className="text-secondary-blue text-2xl">4. Limitação de Responsabilidade</h2>
+                <p className="text-white/90 text-xl">Os conteúdos possuem caráter informativo e educacional. A Impacte Brasil não garante
+                  resultados financeiros, operacionais ou estratégicos decorrentes da aplicação das
+                  informações.
+                </p>
+
+                <h2 className="text-secondary-blue text-2xl">5. Comunicações</h2>
+                <p className="text-white/90 text-xl">Ao fornecer seus dados, o usuário autoriza o envio de comunicações institucionais e de
+                conteúdo, podendo cancelar a qualquer momento.</p>
+
+                <h2 className="text-secondary-blue text-2xl">6. Alterações</h2>
+                <p className="text-white/90 text-xl">Os Termos podem ser alterados sem aviso prévio. O uso contínuo implica aceitação.</p>
+              </div>
+            </LayoutDesktop>
+          </LayoutContent>
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 }
 
