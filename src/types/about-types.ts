34290e9f9432ec646ed42ce5113b48e872e46569
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-27 13:13:07 -0300

    refactor: edit quality images

--- src/types/about-types.ts
+++ src/types/about-types.ts
@@ -1,10 +1,8 @@
 interface SolutionsProps {
-  id?: number
-  icon: string
-  title: string
-  description: string
+  id?: number;
+  icon: string;
+  title: string;
+  description: string;
 }
 
-export type {
-  SolutionsProps
-}
+export type { SolutionsProps };
