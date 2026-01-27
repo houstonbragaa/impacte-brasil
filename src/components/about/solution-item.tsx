9381d0eb55c88dee8654ab36667849be275cc57f
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-27 12:41:07 -0300

    refactor: edit images solutions quality

--- src/components/about/solution-item.tsx
+++ src/components/about/solution-item.tsx
@@ -7,9 +7,12 @@ const SolutionItem = ({ icon, title, description, className = "" }: SolutionsPro
       <div className="bg-white rounded-3xl p-8 h-full w-full flex gap-4 flex-col justify-center items-center">
         <Image 
           src={icon}
-          width={50}
-          height={50}
-          alt={icon}
+          width={128}
+          height={128}
+          className="w-[50px] h-[50px]"
+          sizes="50px"
+          quality={100}
+          alt={title}
         />
         <h2 className="font-bold text-xl text-center">{title}</h2>
         <p className='text-sm opacity-70 text-center'>{description}</p>
